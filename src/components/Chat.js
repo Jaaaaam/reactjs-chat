c24f5b0c0f6968268f116c6adbf3112d45f04d31
Jaaaaam <pjdeesilvestre@gmail.com>
2019-01-23 09:22:37 +0800

    added styling

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -2,10 +2,16 @@ import React, { Component } from 'react'
 
 export default class Chat extends Component {
   render() {
-    return (
-      <div className="chat">
-        
+  return (
+    <div className="chat">
+      <div className="chat-response">
+        <i class="material-icons">
+          attach_file
+        </i>
+        <textarea  />
+        <p>Send</p>
       </div>
-    )
+    </div>
+  )
   }
 }
