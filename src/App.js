09b42b0abd6cda0e2d6c14db14b0e60468847450
Jam Silvestre <pjdeesilvestre@gmail.com>
2019-01-27 09:10:47 +0800

    addded socket

--- src/App.js
+++ src/App.js
@@ -3,9 +3,16 @@ import NavBar from './components/NavBar'
 import SideBar from './components/SideBar'
 import Chat from './components/Chat'
 import './assets/sass/style.scss';
+import socketIOClient from 'socket.io-client'
 
 class App extends Component {
+  state = {
+    endpoint: 'http://localhost:4000/'
+  }
+
   render() {
+    const { endpoint } = this.state
+    const socket = socketIOClient(endpoint)
     return (
       <Fragment>
       <NavBar />
